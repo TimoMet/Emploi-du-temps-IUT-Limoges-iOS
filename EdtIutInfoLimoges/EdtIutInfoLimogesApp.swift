776c2b2a759637c241be24b11b3d65c8eda17e84
Timothée Métais <timotheemetais@wifi-eduroam-etud-235-109.unilim.fr>
2022-10-11 23:43:01 -0700

    Initial commit

---
+++ EdtIutInfoLimoges/EdtIutInfoLimogesApp.swift
@@ -0,0 +1,18 @@
+//
+//  EdtIutInfoLimogesApp.swift
+//  EdtIutInfoLimoges
+//
+//  Created by Timothée Métais on 11/10/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct EdtIutInfoLimogesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
